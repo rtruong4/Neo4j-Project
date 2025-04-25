eb8e295833c86c4367ae80c7f85c654fde7dafdd
Ryan Truong <rtruong63@gmail.com>
2025-04-25 16:22:32 -0700

    added emails and passwords to dataset

---
+++ add_email.py
@@ -0,0 +1,15 @@
+import json
+
+
+with open('data.json', 'r', encoding='utf-8') as f:
+    users = json.load(f)
+
+
+for user in users:
+    
+    username_clean = user["userName"].lstrip('@')
+    user["email"] = f"{username_clean}@email.com"
+
+
+with open('updated_data.json', 'w', encoding='utf-8') as f:
+    json.dump(users, f, indent=2, ensure_ascii=False)
---
+++ add_password.py
@@ -0,0 +1,13 @@
+import json
+
+
+with open('data.json', 'r', encoding='utf-8') as f:
+    users = json.load(f)
+
+
+for user in users:
+    user["password"] = "password"
+
+
+with open('updated_data.json', 'w', encoding='utf-8') as f:
+    json.dump(users, f, indent=2, ensure_ascii=False)
